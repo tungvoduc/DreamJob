c1e6e846fc6f8544357a6ad92a5c7b823059d55c
Tung Vo <tung.vo@elisa.fi>
2019-03-03 11:03:55 +0200

    implement ProfileCoordinator

---
+++ DreamJob/Coordinators/ProfileCoordinator/ProfileCoordinator.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileCoordinator.swift
+//  DreamJob
+//
+//  Created by Vo Tung on 03/03/2019.
+//  Copyright © 2019 Vo Tung. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCoordinator: BaseCoordinator {
+    
+    var profileViewController: ProfileViewController
+    
+    init(viewModel: ProfileViewModelType) {
+        profileViewController = ProfileViewController(viewModel: viewModel)
+        let navigationController = UINavigationController(rootViewController: profileViewController)
+        super.init(router: Router(navigationController: navigationController))
+    }
+    
+}
