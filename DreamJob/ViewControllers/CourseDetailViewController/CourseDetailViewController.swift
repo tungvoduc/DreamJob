fbfa2c3535224b9de1b0a4bc6ee6c64c27fa6ed9
Tung Vo <tung.vo@elisa.fi>
2019-03-09 02:47:55 +0200

    add course action button to course detail view

--- DreamJob/ViewControllers/CourseDetailViewController/CourseDetailViewController.swift
+++ DreamJob/ViewControllers/CourseDetailViewController/CourseDetailViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import RxSwift
 
 class CourseDetailViewController: UIViewController {
     @IBOutlet weak var creditNumberLabel: UILabel!
     @IBOutlet weak var skillsLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var prerequisitesLabel: UILabel!
+    @IBOutlet weak var actionButton: UIButton!
     
     private(set) var viewModel: CourseDetailViewModelType!
+    
+    private let disposeBag = DisposeBag()
     
     init(viewModel model: CourseDetailViewModelType) {
         viewModel = model
@@ -26,6 +31,14 @@ class CourseDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        actionButton.layer.cornerRadius = 5
+        actionButton.layer.masksToBounds = true
+        actionButton.setTitle("Add course to profile", for: .normal)
+        actionButton.setTitle("Remove course from profile", for: .selected)
+        actionButton.setTitleColor(UIColor.white, for: [])
+        actionButton.setBackgroundImage(UIImage.imageWithColor(UIColor.blue, size: CGSize(width: 1, height: 1)), for: .normal)
+        actionButton.setBackgroundImage(UIImage.imageWithColor(UIColor.red, size: CGSize(width: 1, height: 1)), for: .selected)
 
         // Do any additional setup after loading the view.
         populateData()
@@ -36,6 +49,21 @@ class CourseDetailViewController: UIViewController {
         creditNumberLabel.text = viewModel.creditNumberString
         skillsLabel.text = viewModel.skillString
         descriptionLabel.text = viewModel.courseDescription
+        prerequisitesLabel.text = viewModel.prerequisitesString
+        
+        viewModel.didCompleteCourse
+            .do(onNext: { completed in
+                print(completed)
+            })
+            .bind(to: actionButton.rx.isSelected)
+            .disposed(by: disposeBag)
     }
 
+    @IBAction func acitonButtonTapped(_ sender: Any) {
+        if actionButton.isSelected {
+            viewModel.removeCourse.onNext(())
+        } else {
+            viewModel.addCourse.onNext(())
+        }
+    }
 }
--- DreamJob/ViewControllers/CourseDetailViewController/CourseDetailViewModel/CourseDetailViewModel.swift
+++ DreamJob/ViewControllers/CourseDetailViewController/CourseDetailViewModel/CourseDetailViewModel.swift
@@ -15,9 +15,22 @@ protocol CourseDetailViewModelType: CourseBasedViewModelType {
     var creditNumberString: String { get }
     var skillString: String { get }
     var courseDescription: String { get }
+    var prerequisitesString: String { get }
+    var didCompleteCourse: Observable<Bool> { get }
+    
+    var addCourse: AnyObserver<Void> { get }
+    var removeCourse: AnyObserver<Void> { get }
 }
 
 struct CourseDetailViewModel: CourseDetailViewModelType {
+    
+    var removeCourse: AnyObserver<Void>
+    
+    var addCourse: AnyObserver<Void>
+    
+    var didCompleteCourse: Observable<Bool>
+    
+    var prerequisitesString: String
     
     var name: String
     
@@ -29,13 +42,19 @@ struct CourseDetailViewModel: CourseDetailViewModelType {
     
     let course: Course
     
-    init(course: Course) {
+    let profile: Profile
+    
+    private let disposeBag = DisposeBag()
+    
+    init(course: Course, profile: Profile) {
         self.course = course
+        self.profile = profile
         
         name = course.name ?? ""
         
         creditNumberString = "Number of credits: \(course.credits)"
         
+        // Skills
         let skillsPrefix = "Skills: "
         if let unsortedSkills = course.skills?.allObjects as? [Skill], !unsortedSkills.isEmpty {
             let skills = unsortedSkills.compactMap{ $0.name }.sorted() // Sorted by name
@@ -44,12 +63,42 @@ struct CourseDetailViewModel: CourseDetailViewModelType {
             skillString = skillsPrefix + "Not available"
         }
         
+        // Description
         if let courseDescription = course.courseDescription, !courseDescription.isEmpty {
             self.courseDescription = "Description: \(courseDescription)"
         } else {
             self.courseDescription = "Description: Not available"
         }
         
+        // Prerequisites
+        let prerequisitesPrefix = "Prerequisites: "
+        if let unsortedSkills = course.requiredSkills?.allObjects as? [Skill], !unsortedSkills.isEmpty {
+            let skills = unsortedSkills.compactMap{ $0.name }.sorted() // Sorted by name
+            prerequisitesString = prerequisitesPrefix + skills.joined(separator: ", ")
+        } else {
+            prerequisitesString = prerequisitesPrefix + "Not available"
+        }
+        
+        didCompleteCourse = profile.rx.didCompleteCourse(course)
+        
+        let dataStack: CoreDataStack = DataStack.shared
+        let addCourse = PublishSubject<Void>()
+        self.addCourse = addCourse.asObserver()
+        addCourse.asObservable()
+            .subscribe(onNext: { _ in
+                profile.addToCompletedCourses(course)
+                dataStack.saveContext()
+            })
+            .disposed(by: disposeBag)
+        
+        let removeCourse = PublishSubject<Void>()
+        self.removeCourse = removeCourse.asObserver()
+        removeCourse.asObservable()
+            .subscribe(onNext: { _ in
+                profile.removeFromCompletedCourses(course)
+                dataStack.saveContext()
+            })
+            .disposed(by: disposeBag)
     }
     
 }
